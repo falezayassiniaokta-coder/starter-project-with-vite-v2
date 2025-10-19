afae19a715f5906a7ed8c7ce82d96ad9a0419542
Faleza <faleza.yassinia.okta@gmail.com>
2025-10-19 23:10:00 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/starter-project-with-vite-v2/sw.js', { scope: '/starter-project-with-vite-v2/' })})}
